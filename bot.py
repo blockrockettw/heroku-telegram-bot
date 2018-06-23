93a9ea9e0e503f4387d6282d60b2618a4159a0e2
BlockRocket 區塊火箭 <39630127+blockrockettw@users.noreply.github.com>
2018-06-23 13:33:05 +0800

    Second Attempt to create echo bot with Heorku

--- bot.py
+++ bot.py
@@ -1,55 +1,20 @@
-# -*- coding: utf-8 -*-
-import redis
 import os
-import telebot
-# import some_api_lib
-# import ...
-import logging
-import telegram
-from telegram.error import NetworkError, Unauthorized
-from time import sleep
+from telegram.ext import Updater, MessageHandler, Filters
 
-# Example of your code beginning
-#           Config vars
 token = os.environ['TELEGRAM_TOKEN']
-update_id = None
+PORT = int(os.environ.get('PORT', '5000'))
 
-def main():
-    """Run the bot."""
-    global update_id
-    # Telegram Bot Authorization Token
-    bot = telebot.TeleBot(token)
-
-    # get the first pending update_id, this is so we can skip over it in case
-    # we get an "Unauthorized" exception.
-    try:
-        update_id = bot.get_updates()[0].update_id
-    except IndexError:
-        update_id = None
-
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-    while True:
-        try:
-            echo(bot)
-        except NetworkError:
-            sleep(1)
-        except Unauthorized:
-            # The user has removed or blocked the bot.
-            update_id += 1
+def echo(bot, update):
+    update.message.reply_text('Bot answer: ' + update.message.text)
 
 
-def echo(bot):
-    """Echo the message the user sent."""
-    global update_id
-    # Request updates after the last update_id
-    for update in bot.get_updates(offset=update_id, timeout=10):
-        update_id = update.update_id + 1
+updater = Updater(TOKEN)
 
-        if update.message:  # your bot can receive updates without messages
-            # Reply to the message
-            update.message.reply_text(update.message.text)
+# add handlers
+updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
 
-
-if __name__ == '__main__':
-    main()
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+updater.bot.setWebhook("https://telegrambotexample31337.herokuapp.com/" + TOKEN)
+updater.idle()
